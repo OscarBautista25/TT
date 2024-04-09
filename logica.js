a4f6a238ebbc745be5e281e9d609775783502c24
Oscar Bautista <ingenierobautistaoscar@gmail.com>
2024-04-08 21:04:55 -0500

    Segundos cambios en pruebas

--- logica.js
+++ logica.js
@@ -1,3 +1,4 @@
 alert("Esto es una ventana emergente")
 console.log("Se agrega segunda versión desde local")
 console.log("se agrega cambio en rama pruebas")
+console.log("segundos cambios en rama pruebas")
